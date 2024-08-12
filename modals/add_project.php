b6515e428b5dac791fda3b17702c1e98ee1b61ff
lemon8de <jayandrew.minon@gmail.com>
2024-08-12 11:58:26 +0800

    ui

--- modals/add_project.php
+++ modals/add_project.php
@@ -3,6 +3,7 @@
     <div class="modal-dialog modal-xl" style="min-width:98%;">
         <div class="modal-content" style="padding:10px; margin:10px;">
             <!-- content start -->
+            <!-- <div class="container"> -->
                 <div class="row">
                     <div class="col-4">
                         <label>General</label>
@@ -17,6 +18,32 @@
                             <div class="col-sm-8">
                                 <input type="text" class="form-control">
                             </div>
+                        </div>
+                        <div class="form-group">
+                            <label>Product Description</label>
+                            <textarea id="" name="" rows="4" style="width: 100%;"></textarea>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Project Category</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select Project Category</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">TDE Status</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select TDE Status</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">SUB BU</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select SUB BU</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label>Notes</label>
+                            <textarea id="" name="" rows="4" style="width: 100%;"></textarea>
                         </div>
                     </div>
                     <div class="col-4">
@@ -39,6 +66,50 @@
                                 <input type="text" class="form-control">
                             </div>
                         </div>
+                        <label>Key Personnel</label>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Primary TDE</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select Primary TDE</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Primary PE</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select Primary PE</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Releasing TDE</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select Releasing TDE</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Releasing PE</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select Releasing PE</option>
+                            </select>
+                        </div>
+                        <label>Equipment</label>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Tester</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select Tester</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Handler</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select Handler</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Prober</label>
+                            <select class="col-sm-8 form-control" id="" name="">
+                                <option disabled selected value="">Select Prober</option>
+                            </select>
+                        </div>
                     </div>
                     <div class="col-4">
                         <label>Date Created</label>
@@ -53,8 +124,107 @@
                                 <input type="text" class="form-control">
                             </div>
                         </div>
+
+                        <label>Critical Dates</label>
+                        <div class="row">
+                            <div class="col-4"></div>
+                            <div class="col-4">
+                                <label>Initial</label>
+                            </div>
+                            <div class="col-4">
+                                <label>Re-Commit</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                <label>Silicon Availability</label>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                <label>Char Lot Availability</label>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                <label>Transfer Package</label>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                <label>IB Lot Availability</label>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-4">
+                                <label>Target Release</label>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="form-group">
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-6 col-form-label">TDE Activities Completed</label>
+                            <div class="col-sm-6">
+                                <input type="date" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-sm-6 col-form-label">Actual Release</label>
+                            <div class="col-sm-6">
+                                <input type="date" class="form-control">
+                            </div>
+                        </div>
+                    </div>
                 </div>
-             </div>
+            <!-- </div> -->
             <!-- content end -->
             <div class="modal-footer justify-content-between">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
